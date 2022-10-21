33ff45cefa39fe65dc70a5281d9aeea89d4bf587
MeryAmun <chebesiawah@gmail.com>
2022-10-21 12:54:12 +0100

    db json server live

--- src/features/RTkApi/apiSlice.js
+++ src/features/RTkApi/apiSlice.js
@@ -1,12 +1,18 @@
-// Import the RTK Query methods from the React-specific entry point
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// Define our single API slice object
+// GET    /posts
+// GET    /posts/1
+// POST   /posts
+// PUT    /posts/1
+// PATCH  /posts/1
+// DELETE /posts/1
+const baseUrl = 'http://localhost:3001'
 export const apiSlice = createApi({
   // The cache reducer expects to be added at `state.api` (already default - this is optional)
   reducerPath: 'api',
   // All of our requests will have URLs starting with '/fakeApi'
-  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  baseQuery: fetchBaseQuery(baseUrl),
   // The "endpoints" represent operations and requests for this server
   endpoints: builder => ({
     // The `getPosts` endpoint is a "query" operation that returns data
@@ -17,5 +23,5 @@ export const apiSlice = createApi({
   })
 })
 
-// Export the auto-generated hook for the `getPosts` query endpoint
+
 export const { useGetPostsQuery } = apiSlice
